64112a9ccb426f862e3f4485692b134abf420a4c
stefaleon <stefaleon@gmail.com>
2019-12-06 15:40:00 +0200

    use Consumer to get the context in the Button component (instead of contextType and this.context)

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
-  static contextType = LanguageContext;
-
   render() {
-    const text = this.context === "english" ? "Submit" : "Υποβολή";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Υ  ποβολή")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
